fc8dbc40a219c2787138ebfa760aea6a8eb75cd6
frans <franzferdinand.kbarek@gmail.com>
2022-09-03 17:43:10 +0900

    pending

--- roman-to-integer(easy).js
+++ roman-to-integer(easy).js
@@ -3,23 +3,19 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-  const romanSymbol = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-  for (let i = 0; i < romanSymbol.length; i++) {
-    console.log(i);
-  }
-  console.log(romanSymbol);
-  return romanSymbol == s ? true : false;
-};
+  const roman = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
 
+  console.log(roman.get("I"));
+  console.log(roman.get("M"));
+  console.log(roman.has("V"));
+};
 // test case
-console.log(romanToInt(10));
-console.log(romanToInt(1990));
-console.log(romanToInt(2));
+console.log(romanToInt("I"));
