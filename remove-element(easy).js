69b16b41be4eb2c756e2431eb8b46afafde5efd6
frans <franzferdinand.kbarek@gmail.com>
2022-12-03 20:20:03 +0900

    remove element solved

---
+++ remove-element(easy).js
@@ -0,0 +1,13 @@
+const removeElement = (nums, val) => {
+  let idx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[idx] = nums[i];
+      idx++;
+    }
+  }
+  return idx;
+};
+// test case
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
