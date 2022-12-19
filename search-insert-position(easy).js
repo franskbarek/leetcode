06ea47080c093f08633d6d9318acb5b4a2b26aca
frans <franzferdinand.kbarek@gmail.com>
2022-12-19 10:24:23 +0900

    search insert solved

--- search-insert-position(easy).js
+++ search-insert-position(easy).js
@@ -3,8 +3,26 @@
  * @param {number} target
  * @return {number}
  */
+
 const searchInsert = (nums, target) => {
-  return nums.filter((x) => x < target).length;
+  // #solution2 👍 damn optimize: 0(log n) 85% beats
+  let lo = 0;
+  let hi = nums.length - 1;
+
+  while (lo <= hi) {
+    let mid = Math.floor((hi + lo) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
+    }
+  }
+  return lo;
+
+  // #solution1
+  // return nums.filter((x) => x < target).length;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 5));
